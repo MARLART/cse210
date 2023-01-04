c2a5c84f9b8297e4e44a24f12af0884bfabc0b65
MARLART <les22005@byui.edu>
2023-01-04 13:28:11 +1300

    prep 2 completed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,65 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string grade;
+        float calcSign;
+        string sign;
+
+        Console.Write("What is your grade percentage? ");
+        string textPercent = Console.ReadLine();
+        float percent = float.Parse(textPercent);
+        calcSign = percent % 10;
+
+        if (percent >= 90)
+        {
+            grade = "A";
+        }
+        else if (percent>= 80)
+        {
+            grade = "B";
+        }
+        else if (percent>= 70)
+        {
+            grade = "C";
+        }
+        else if (percent>= 60)
+        {
+            grade = "D";
+        }
+        else 
+        {
+            grade = "F";
+        }
+
+        if (percent <= 60 || percent >=97)
+        {
+            sign = "";
+        }
+        else
+        {
+            if (calcSign < 3)
+            {
+                sign = "-";
+            }
+            else if (calcSign >= 7)
+            {
+                sign = "+";
+            }
+            else 
+            {
+                sign = "";
+            }
+        }
+        
+
+        Console.WriteLine($"You earned a {grade}{sign}.");
+        if(percent >= 70)
+        {
+            Console.WriteLine("Congratulations you passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("You failed the course, better luck next time.");
+        }
     }
 }
