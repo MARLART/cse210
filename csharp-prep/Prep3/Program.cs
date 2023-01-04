7b9f5a26423b263d8cdf2739d7a6613a8903a61b
MARLART <les22005@byui.edu>
2023-01-04 14:18:24 +1300

    prep 3 completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string cont = "yes";
+        string guess = "0";
+
+        while (cont == "yes")
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+            int noGuesses = 0;
+            //Console.WriteLine(magicNumber);
+
+            Console.WriteLine("The magic number is between 1 and 100");
+            Console.Write("What is your guess? ");
+            guess = Console.ReadLine();
+            noGuesses +=1;
+            int guessNum = int.Parse(guess);
+
+            while(!(magicNumber == guessNum))
+            {
+                if (magicNumber > guessNum)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("Lower");
+                }
+                Console.Write("What is your guess? ");
+                guess = Console.ReadLine();
+                noGuesses +=1;
+                guessNum = int.Parse(guess);
+            }
+
+            Console.WriteLine("You are correct.");
+            Console.WriteLine($"You took {noGuesses} guesses.");
+            Console.Write("Would you like to play again? ");
+            cont = Console.ReadLine();
+        }
+        
+        Console.WriteLine("Thank you for playing.");
     }
 }
