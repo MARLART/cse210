0b2e8d2dc1298dad42106b0c31c10377333bd12f
MARLART <les22005@byui.edu>
2023-01-24 17:02:04 +1300

    learning 4 done

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment1 = new Assignment("Doggy", "Eating");
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment math1 = new MathAssignment("pusspuss", "purring", "5", "5.1 - 5.6");
+        Console.WriteLine(math1.GetSummary());
+        Console.WriteLine(math1.GetHomeworkList());
+
+        WritingAssignment writing1 = new WritingAssignment("Mummy", "Tired", "How to sleep more.");
+        Console.WriteLine(writing1.GetSummary());
+        Console.WriteLine(writing1.GetWritingInformation());
     }
 }
