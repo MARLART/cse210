37472d42d274a58af3bf9d8986cdb72d0937c03e
MARLART <les22005@byui.edu>
2023-01-25 13:56:54 +1300

    menu written

---
+++ prove/Develop04/Menu.cs
@@ -0,0 +1,27 @@
+using System;
+
+class Menu
+{
+    private string _menuWelcome ="Welcome to the Mindfulness Program.";
+    private string _menu0 = "Menu Options:";
+    private string _menu1 = "1. Start breathing activity";
+    private string _menu2 = "2. Start reflecting activity";
+    private string _menu3 = "3. Start listing activity";
+    private string _menu4 = "4. Quit";
+    private string _menu5 = "Select a choice from the menu: ";
+    public Menu()
+    {
+        DisplayMenu();
+    }
+
+    private void DisplayMenu()
+    {
+        Console.WriteLine(_menuWelcome);
+        Console.WriteLine(_menu0);
+        Console.WriteLine(_menu1);
+        Console.WriteLine(_menu2);
+        Console.WriteLine(_menu3);
+        Console.WriteLine(_menu4);
+        Console.Write(_menu5);
+    }
+}
