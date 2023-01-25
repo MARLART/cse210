b5247836059aa21f3991fb076945e247fdcfb920
MARLART <les22005@byui.edu>
2023-01-25 14:07:22 +1300

    basic program class

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,8 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
-        Menu menu1 = new Menu();
+        string _menuOption = "0";
+
+        do
+        {
+            //Display the menu
+            Menu menu1 = new Menu();
+
+            //Display the menu response
+            _menuOption = Console.ReadLine();
+
+            //option 1 breathing
+            if (_menuOption == "1")
+            {
+                Console.WriteLine("I am breathing");
+            }
+            else if (_menuOption == "2")
+            {
+                Console.WriteLine("I am reflecting");
+            }
+
+            else if (_menuOption == "3")
+            {
+                Console.WriteLine("I am listing");
+            }
+        } while (_menuOption != "4");
+
+        Console.WriteLine("Thank you Good Bye");
         
+
     }
 }
