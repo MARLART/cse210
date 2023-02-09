6b73b0dde84e95ba0216200e6adabbf79f0b5adc
MARLART <les22005@byui.edu>
2023-02-09 15:38:56 +1300

    more tidy

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.IO;
+using System.Text;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello FinalProject World!");
+        string _choice = "0";
+
+        while (_choice != "4")
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("* Should I buy that Car? *");
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("1. Do I buy the car now with a loan while it is discounted or continue to save?"); 
+            Console.WriteLine("2. Will the electric / hybrid car save me money?");
+            Console.WriteLine("3. Compare all finance and a car options");
+            Console.WriteLine("4. QUIT");
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("What decision do you need help with?  ");     
+        }
+        
     }
 }
